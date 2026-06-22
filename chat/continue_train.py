e26ceaa77faf1cadcc60cba0d54c253b3bd9d686
harptwzx <wzx123_snake@qq.com>
2026-06-22 17:10:49 +0800

    更新 continue_train.py

--- chat/continue_train.py
+++ chat/continue_train.py
@@ -10,7 +10,7 @@ with open(vocab_path, 'r', encoding='utf-8') as f:
 id2token = {int(v): k for k, v in vocab.items()}
 vocab_size = len(vocab)
 
-saved_model_path = './saved_model'
+saved_model_path = './saved_model/gpu_language_modle'
 loaded = tf.saved_model.load(saved_model_path)
 
 infer = loaded.signatures['serving_default']
