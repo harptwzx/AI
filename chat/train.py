a01f988599f5a746d7a2a9d440c84ea04342a422
harptwzx <wzx123_snake@qq.com>
2026-07-12 08:06:01 +0000

    更新 train.py

--- chat/train.py
+++ chat/train.py
@@ -38,8 +38,8 @@ class TrainingConfig:
 
     # 训练配置
     batch_size = 64
-    epochs = 20
-    learning_rate = 1e-3        # 固定学习率
+    epochs = 10
+    learning_rate = 3e-4        # 固定学习率
     # warmup_steps = 2000       # 不再使用
     # max_train_steps = 500000  # 不再使用
 
